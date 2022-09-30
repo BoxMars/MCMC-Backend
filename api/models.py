2a561cdb709a8149365b5cf6df5a150a8f8786d7
Box <me@boxz.dev>
2022-09-30 10:31:09 +0000

    new file:   db.sqlite3 	modified:   mcmc/__pycache__/settings.cpython-310.pyc

--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class token(models.Model):
+    token=models.CharField(max_length=64, default="")
+    notes=models.TextField()
+
+class news(models.Model):
+    title=models.CharField(max_length=1024,default="")
+    pic_url=models.CharField(max_length=1024,default="")
+    news_url=models.CharField(max_length=1024,default="")
+    submit_by=models.CharField(max_length=1024,default="")
+
+    def info(self):
+        res={
+            'title':self.title,
+            'pic_url':self.pic_url,
+            'news_url':self.news_url
+        }
+        return res
+class head_num(models.Model):
+    num=models.IntegerField(default=6)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+import json
+import api.models as models
 
 # Create your views here.
+def get_news_list(request):
+    res={
+        'data':[],
+        'num':0,
+    }
+    news_list=models.news.objects.all()
+    for i in news_list:
+        res['data'].append(i.info())
+    res['num']=models.head_num.objects.all().first().num
+    return JsonResponse(json.dumps(res))
+
+
+def submit_news(request):
+    token=request.POST['token']
+    res={
+        'code':-1,
+        'message':"Error"
+    }
+    is_verify=False
+    for i in models.token.objects.all():
+        if i.token==token:
+            is_verify=True
+    if not is_verify:
+        return JsonResponse(json.dumps(res))
+    title=request.POST['title']
+    pic_url=request.POST['pic_url']
+    news_url=request.POST['news_url']
+    news=models.news(title=title,pic_url=pic_url,news_url=news_url,submit_by=tokenp)
+    news.save()
+    res={
+        'code':1,
+        'message':"Success"
+    }
+    return JsonResponse(json.dumps(res))
+
+def submit_num(request):
+    token=request.POST['token']
+    res={
+        'code':-1,
+        'message':"Error"
+    }
+    is_verify=False
+    for i in models.token.objects.all():
+        if i.token==token:
+            is_verify=True
+    if not is_verify:
+        return JsonResponse(json.dumps(res))
+    num=request.POST['num']
+    num_record=models.head_num.objects.all().first()
+    num_record.num=num
+    num_record.save()
+    res={
+        'code':1,
+        'message':"Success"
+    }
+    return JsonResponse(json.dumps(res))
